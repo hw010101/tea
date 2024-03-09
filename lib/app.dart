a53f4d50b117702c4fcac9bd6ab4cdae36a65e17
hw010101 <119528789+hw010101@users.noreply.github.com>
2024-03-09 20:05:22 +0300

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'features/number_trivia/presentation/pages/number_trivia_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter architect templete",
+      theme: ThemeData(
+        primaryColor: Colors.green.shade800,
+      ),
+      home: const NumberTriviaPage(),
+    );
+  }
+}
